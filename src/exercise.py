2244db3fcff2753ae8031f1ccb4aa0ebf3b77a90
Tianhao <30402585+SE-starshippilot@users.noreply.github.com>
2022-09-29 20:31:23 +0800

    fixed typo

--- src/exercise.py
+++ src/exercise.py
@@ -1,9 +1,9 @@
-def avg_list(numbsers: list)->int:
+def avg_list(numbers: list)->int:
     #write your code below this line
     pass
 
 def main():
-    print(avg_list([1, 2, 3, 4])
+    print(avg_list([1, 2, 3, 4]))
     pass
 
 if __name__ == '__main__':
